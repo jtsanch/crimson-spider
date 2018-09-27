66366ab59d910f463a0de6324472e686569d47d3
Travis Sanchez <travis.sanchez@roche.com>
2018-09-27 16:56:01 -0700

    fixed persitence of login issue

--- src/app/components/option/option.component.ts
+++ src/app/components/option/option.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { User } from 'firebase';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'app-option',
     templateUrl: './option.component.html',
     styleUrls: ['./option.component.css']
 })
-export class OptionComponent implements OnInit {
+export class OptionComponent implements  OnInit{
 
     public user: User;
-    constructor(
-        private _authService: AuthService,
-    ) {
-    }
+    constructor(private _authService: AuthService,) {}
 
     ngOnInit() {
-        this.user = this._authService.getUser();
-    }
 
+        this._authService.getUser()
+            .then((user) => {
+                this.user = user;
+            });
+    }
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { User } from 'firebase';
+import { P } from '@angular/core/src/render3';
 
 @Injectable()
 export class AuthService {
 
     private _user: User;
 
-    constructor(public afAuth: AngularFireAuth) {
-        this._user = null;
-    }
+    constructor(public afAuth: AngularFireAuth) {}
 
     public login(email: string, password: string): Promise<boolean> {
         const promise = new Promise<boolean>((resolve, reject) => {
@@ -43,7 +42,16 @@ export class AuthService {
         return !!this._user;
     }
 
-    public getUser(): User {
-        return this._user;
+    public getUser(): Promise<User> {
+        return new Promise((resolve, reject) => {
+            this.afAuth.auth.onAuthStateChanged(user => {
+                if (user) {
+                    this._user = user;
+                    resolve(user);
+                } else {
+                    reject(null);
+                }
+            });
+        });
     }
 }
